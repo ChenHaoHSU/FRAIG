83ab03cb323bb5dc8f11e938f021362a3df024de
ChenHao <b03505028@ntu.edu.tw>
2018-01-26 03:53:03 +0800

    cirStrash.h format

--- src/cir/cirStrash.h
+++ src/cir/cirStrash.h
@@ -3,7 +3,7 @@
   PackageName  [ cir ]
   Synopsis     [ Classes for Strash command. 
                  CirStrashS is the key-value pair for HashSet implementation.
-                 CirStrashM is the key for HashSet implementation. ]
+                 CirStrashM is the key for HashMap implementation. ]
   Author       [ Chen-Hao Hsu ]
   Date         [ 2018/1/25 created ]
 ****************************************************************************/
@@ -21,35 +21,35 @@ using namespace std;
 class CirStrashS
 {
 public:
-	CirStrashS(CirGate* g = 0) : _gate(g) {}
-	~CirStrashS() {}
+   CirStrashS(CirGate* g = 0) : _gate(g) {}
+   ~CirStrashS() {}
 
-	/* Key */
-	size_t operator () () const {
-		return _gate->fanin0() ^ _gate->fanin1();
-		// return _gate->fanin0() + _gate->fanin1();
-	}
+   /* Key */
+   size_t operator () () const {
+         return _gate->fanin0() ^ _gate->fanin1();
+      // return _gate->fanin0() + _gate->fanin1();
+   }
 
-	/* Key comparison function */
-	bool operator == (const CirStrashS& c) const {
-		if (_gate->fanin0() == c.gate()->fanin0())
-			return (_gate->fanin1() == c.gate()->fanin1());
-		if (_gate->fanin0() == c.gate()->fanin1())
-			return (_gate->fanin1() == c.gate()->fanin0());
-		return false;
-	}
+   /* Key comparison function */
+   bool operator == (const CirStrashS& c) const {
+      if (_gate->fanin0() == c.gate()->fanin0())
+         return (_gate->fanin1() == c.gate()->fanin1());
+      if (_gate->fanin0() == c.gate()->fanin1())
+         return (_gate->fanin1() == c.gate()->fanin0());
+      return false;
+   }
 
-	CirStrashS& operator = (const CirStrashS& c) {
-		_gate = c.gate();
-		return (*this);
-	}
+   CirStrashS& operator = (const CirStrashS& c) {
+      _gate = c.gate();
+      return (*this);
+   }
 
-	/* Basic functions */
-	CirGate* gate() const { return _gate; }
-	void     setGate(CirGate* g) { _gate = g; }
+   /* Basic functions */
+   CirGate* gate() const { return _gate; }
+   void     setGate(CirGate* g) { _gate = g; }
 
 private:
-	CirGate* _gate;
+   CirGate* _gate;
 };
 
 /******************************
@@ -58,30 +58,30 @@ private:
 class CirStrashM
 {
 public:
-	CirStrashM(CirGate* g = 0) : _gate(g) {}
-	~CirStrashM() {}
+   CirStrashM(CirGate* g = 0) : _gate(g) {}
+   ~CirStrashM() {}
 
-	/* Key */
+   /* Key */
    size_t operator() () const { 
-		return _gate->fanin0() ^ _gate->fanin1();
-		// return _gate->fanin0() + _gate->fanin1();
-	}
-
-	/* Key comparison function */
-   bool operator == (const CirStrashM& c) const { 
-		if (_gate->fanin0() == c.gate()->fanin0())
-			return (_gate->fanin1() == c.gate()->fanin1());
-		if (_gate->fanin0() == c.gate()->fanin1())
-			return (_gate->fanin1() == c.gate()->fanin0());
-		return false;
+      return _gate->fanin0() ^ _gate->fanin1();
+      // return _gate->fanin0() + _gate->fanin1();
    }
 
-	/* Basic functions */
-	CirGate* gate() const { return _gate; }
-	void     setGate(CirGate* g) { _gate = g; }
+   /* Key comparison function */
+   bool operator == (const CirStrashM& c) const { 
+      if (_gate->fanin0() == c.gate()->fanin0())
+         return (_gate->fanin1() == c.gate()->fanin1());
+      if (_gate->fanin0() == c.gate()->fanin1())
+         return (_gate->fanin1() == c.gate()->fanin0());
+      return false;
+   }
+
+   /* Basic functions */
+   CirGate* gate() const { return _gate; }
+   void     setGate(CirGate* g) { _gate = g; }
 
 private:
-	CirGate* _gate;
+   CirGate* _gate;
 };
 
 #endif // CIRSTRASH_H
